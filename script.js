31099fd8e77da0f9c686413546a91321327bf69d
David Yang <davidyang@DaviddeMacBook-Pro.local>
2026-02-18 14:31:34 -0800

    Initial portfolio submission

---
+++ script.js
@@ -0,0 +1,23 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('visible');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
